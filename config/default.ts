b91d8d50782a65395f1aa05b55a52150570ec205
Mounir-Bennacer <benphaoris@gmail.com>
2021-12-28 21:20:47 +0000

    removing unnecessary files

--- config/default.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  port: 3000,
-  host: "localhost",
-  db: "mongodb+srv://mounir-2:Rk4ksiMH51GxsNCF@cluster0.abjje.mongodb.net/restaurant-manager-api?retryWrites=true&w=majority",
-  saltRounds: 10,
-};
